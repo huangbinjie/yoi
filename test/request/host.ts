f9386b651fd0bd59ea6e1b1430e0eb3122fe6e88
bingjie.huang <bingjie.huang@shuyun.com>
2017-06-16 18:46:38 +0800

    test

---
+++ test/request/host.ts
@@ -0,0 +1,17 @@
+// import { test } from "ava"
+// import * as http from "http"
+// import { Server } from "../../src/server"
+// import { createRequest, IRequest } from "../../src/request"
+
+// const app = new Server()
+
+// test.cb("req.host", t => {
+// 	app.use((req, res) => {
+// 		console.log(req.host)
+// 		t.is(req.host, "localhost:3000")
+// 		t.end()
+// 	})
+// 		.listen(3000)
+
+// 	http.get("http://localhost:3000")
+// })
---
+++ test/request/ip.ts
@@ -0,0 +1,16 @@
+// import { test } from "ava"
+// import * as http from "http"
+// import { Server } from "../../src/server"
+// import { createRequest, IRequest } from "../../src/request"
+
+// const app = new Server()
+
+// test.cb("req.ip", t => {
+// 	app.use((req, res) => {
+// 		t.is(req.ip, "::ffff:127.0.0.1")
+// 		t.end()
+// 	})
+// 		.listen(3000)
+
+// 	http.get("http://localhost:3000")
+// })
